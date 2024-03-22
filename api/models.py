df369d3c115b984748621a89d7203d6245bc763e
matheus <matheuscosta.tech@gmail.com>
2024-03-21 23:03:12 -0300

    Trying to write the first models

--- api/models.py
+++ api/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Member(models.Model):
+    name = models.CharField(max_length=50)
+    profile_picture = models.ImageField()
+    phone_number = models.CharField(max_length=12)
+    birth_date = models.DateField()
+
+
+class Auth(models.Model):
+    # eis a questão
+    pass
+
+
+class Manager(models.Model):
+    member_info = models.ForeignKey(Member, null=True)
+    auth = models.ForeignKey(Auth)
