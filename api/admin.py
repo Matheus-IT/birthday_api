c8b357967861c322e08e5fc090ae121fd4bc278b
matheus <matheuscosta.tech@gmail.com>
2024-03-23 15:15:39 -0300

    Creating first models for api

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from api.models import Auth, Manager, Member
 
-# Register your models here.
+admin.site.register([Auth, Manager, Member])
--- api/models.py
+++ api/models.py
@@ -1,4 +1,41 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.utils.translation import gettext_lazy as _
+
+
+class AuthManager(BaseUserManager):
+    def create_user(self, email, password, **extra_fields):
+        """
+        Create and return a regular user with an email and password.
+        """
+        if not email:
+            raise ValueError(_('The email field must be provided'))
+        if not password:
+            raise ValueError(_('The password field must be provided'))
+        
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+    def create_superuser(self, email, password, **extra_fields):
+        """
+        Create and return a superuser with an email and password.
+        """
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(email, password, **extra_fields)
+
+
+class Auth(AbstractBaseUser):
+    USERNAME_FIELD = 'email'
+    objects = AuthManager()
+
+    email = models.EmailField(unique=True)
+
+    def __str__(self) -> str:
+        return self.email
 
 
 class Member(models.Model):
@@ -8,11 +45,6 @@ class Member(models.Model):
     birth_date = models.DateField()
 
 
-class Auth(models.Model):
-    # eis a questão
-    pass
-
-
 class Manager(models.Model):
-    member_info = models.ForeignKey(Member, null=True)
-    auth = models.ForeignKey(Auth)
+    member_info = models.OneToOneField(Member, on_delete=models.SET_NULL, null=True)
+    auth = models.OneToOneField(Auth, on_delete=models.CASCADE)
