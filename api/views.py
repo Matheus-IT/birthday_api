49439460ca77b8f76421617b088cca168981e6b5
matheus <matheuscosta.tech@gmail.com>
2024-03-26 11:57:59 -0300

    Login view

--- api/views.py
+++ api/views.py
@@ -6,19 +6,13 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 
-def hello_view(request):
-    return JsonResponse({'msg': 'Salut!'})
-
-
 class LoginView(APIView):
     def post(self, request):
-        email = request.data.get('email')
-        print('email', email)
-        password = request.data.get('password')
-        print('password', password)
+        email = request.data.get("email")
+        password = request.data.get("password")
         user = authenticate(email=email, password=password)
         if user is not None:
             token, created = Token.objects.get_or_create(user=user)
-            return Response({'token': token.key})
+            return Response({"token": token.key})
         else:
-            return Response({'error': 'Invalid credentials'}, status=400)
+            return Response({"error": "Invalid credentials"}, status=400)
