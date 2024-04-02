902bac2a85d4f16ba7c4644f64dd369a4f7ed947
matheus <matheuscosta.tech@gmail.com>
2024-04-01 22:22:26 -0300

    Using viewset to CRUD operations of members

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,13 @@
 from api import views
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+
+router = DefaultRouter()
+router.register(r"members", views.MemberViewSet, basename="member")
 
 
 urlpatterns = [
     path("login/", views.LoginView.as_view(), name="login"),
-    path("members/", views.members_view, name="members"),
+    path("", include(router.urls)),
 ]
--- api/views.py
+++ api/views.py
@@ -1,16 +1,8 @@
-from django.shortcuts import render
-from django.http import JsonResponse
 from django.contrib.auth import authenticate
 from rest_framework.authtoken.models import Token
-from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework.decorators import (
-    api_view,
-    authentication_classes,
-    permission_classes,
-)
 from api.models import Member
 from api.serializers import MemberSerializer
 from rest_framework.viewsets import ModelViewSet
@@ -26,15 +18,6 @@ class LoginView(APIView):
             return Response({"token": token.key})
         else:
             return Response({"error": "Invalid credentials"}, status=400)
-
-
-@api_view(["GET"])
-@authentication_classes([TokenAuthentication])
-@permission_classes([IsAuthenticated])
-def members_view(request):
-    members = Member.objects.all()
-    serializer = MemberSerializer(members, many=True)
-    return Response(serializer.data)
 
 
 class MemberViewSet(ModelViewSet):
