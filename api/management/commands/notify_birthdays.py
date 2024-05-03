81eab94f43c3f44b36fd96d5b9c8d93be59944e4
matheus <matheuscosta.tech@gmail.com>
2024-05-02 22:22:41 -0300

    Adding birthdays of the day

--- api/management/commands/notify_birthdays.py
+++ api/management/commands/notify_birthdays.py
@@ -2,19 +2,30 @@ from django.core.management.base import BaseCommand
 from decouple import config
 import firebase_admin
 from firebase_admin import credentials, messaging
+from api.models import Member
+from datetime import datetime
 
 
 class Command(BaseCommand):
     help = "Sends request to notify users on firebase"
 
     def handle(self, *args, **options):
-
         cred = credentials.Certificate(config("FIREBASE_KEY_PATH"))
         firebase_admin.initialize_app(cred)
 
+        now = datetime.now()
+        birthday_people = Member.objects.filter(
+            birth_date__day=now.day, birth_date__month=now.month
+        )
+
+        notification_body = ""
+
+        for p in birthday_people:
+            notification_body += f"{p.name} \n"
+
         message = messaging.Message(
             notification=messaging.Notification(
-                title="Testando", body="Mensagem enviada do servidor"
+                title="Aniversariantes do dia", body=notification_body
             ),
             topic="birthdays-of-the-day",
         )
