8c60a5fc5ec49178d853218552fad57d0fc656a5
matheus <matheuscosta.tech@gmail.com>
2024-08-31 15:07:44 -0300

    Fix when going to /

--- app/core/urls.py
+++ app/core/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='api/members/')),
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
 ]
